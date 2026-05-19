90874bb588d4c8f1a1079458fe1dd9dadc6d6904
ashniccole <89165773+ashniccole@users.noreply.github.com>
2026-05-19 05:40:27 -0700

    Refactoring code
    Refactoring my crop code to make it better

--- scripts/CropData.cs
+++ scripts/CropData.cs
@@ -3,15 +3,12 @@ using Godot;
 [GlobalClass]
 public partial class CropData : Resource
 {
-	[Export]
-	public string CropName = "";
+	[Export] public string CropName = "";
+	[Export] public float GrowTime = 10.0f;
+	[Export] public Texture2D SeedTexture;
+	[Export] public Texture2D GrownTexture;
 
-	[Export]
-	public float GrowTime = 10.0f;
-
-	[Export]
-	public Texture2D SeedTexture;
-
-	[Export]
-	public Texture2D GrownTexture;
+	// Move scale and offset here instead of hardcoding in Crop.cs
+	[Export] public Vector2 SpriteScale = Vector2.One;
+	[Export] public Vector2 SpriteOffset = Vector2.Zero;
 }
